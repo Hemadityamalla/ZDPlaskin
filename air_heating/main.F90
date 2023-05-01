48744203fe93368d344315645e9617ed351cadd3
Hemaditya <hemadityamalla@gmail.com>
2023-05-01 09:04:55 +0200

    More testing

--- air_heating/main.F90
+++ air_heating/main.F90
@@ -13,12 +13,14 @@ program main
     double precision, parameter :: boltzmann_const = 1.38d-23
     double precision, parameter :: elementary_charge = 1.6d-19
     double precision, parameter :: bar_to_pascal = 1.0d5  
+    double precision, parameter :: ev_to_K = 1.16e4  
+    double precision, parameter :: SI_to_Townsend = 1.0d21
+
     
     double precision :: gas_pressure    = 1.0d0 ! pressure, bar
     double precision :: gas_temperature = 300.0d0  ! temperature, K
     double precision :: gas_density     = 1.0d0 !density in kg/m3
     double precision :: field_amplitude    = 0.8d+6! Applied field in V/m
-    double precision :: SI_to_Townsend = 1.0d21
     double precision :: density_ini_elec = 1.0d13! initial electron density, cm-3
     double precision :: time  = 0.0d0, time_end = 53.0d-9, dtime = 1.0d-10! times, s
     integer          :: i, t_iter, ifile_unit
@@ -53,13 +55,16 @@ program main
         time = time + dtime
         call get_field(field, time)
         reduced_field = field*SI_to_Townsend/(gas_density*1e6)
+        print *, reduced_field
         call ZDPlasKin_set_conditions(REDUCED_FIELD = reduced_field, GAS_HEATING=.true.)
         call ZDPlaskin_write_qtplaskin(time)
         call ZDPlasKin_get_conditions(ELEC_MOBILITY_N=mob)
         call ZDPlasKin_get_conditions(ELEC_POWER_ELASTIC_N=e_power)
+        call ZDPlasKin_get_conditions(REDUCED_FIELD=reduced_field)
         JdotE = elementary_charge*mob*density(species_electrons)*&
             reduced_field*reduced_field
-        print *, time, JdotE, gas_density*e_power*elementary_charge*density(species_electrons)
+        !print *, time, elementary_charge*mob*reduced_field**2, e_power*elementary_charge*gas_density
+        print *, time, mob, reduced_field
         t_iter = t_iter+1
     enddo
 contains
